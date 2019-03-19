6276cd94c16f5c1be5ff6dd4d3ab223f3480e13d
Pol Valls Ortiz <vallsortizpol@gmail.com>
2019-03-19 15:34:42 +0100

    Lock 2 done

--- muty-source-files/lock2.erl
+++ muty-source-files/lock2.erl
@@ -1,74 +1,78 @@
--module(lock1).
+-module(lock2).
 -export([start/1]).
 
 start(MyId) ->
-    {master, 'master@c6s303pc21.fib.upc.es'} ! reg,
+    {master, 'muty@ASUSPol.upc.edu'} ! reg,
     spawn(fun() -> init(MyId) end).
 
-init(Id) ->
+init(MyId) ->
     receive
         {peers, Nodes} ->
-            open(Nodes, Id);
+            open(Nodes, MyId);
         stop ->
             ok
     end.
 
-open(Nodes, Id) ->
+open(Nodes, MyId) ->
     receive
+        %%WORKER WANTS TO ACCESS EXCLUSION ZONE
         {take, Master, Ref} ->
-            Refs = requests(Nodes),
-            wait(Nodes, Master, Refs, [], Ref, Id);
-        {request, From,  Ref} ->
+            Refs = requests(Nodes,MyId),
+            wait(Nodes, Master, Refs, [], Ref, MyId);
+        {request, From,  Ref, Id} ->
             From ! {ok, Ref},
-            open(Nodes);
+            open(Nodes,MyId);
         stop ->
             ok
     end.
 
-requests(Nodes) ->
+%%SEND ALL THE REQUESTS TO PEERS
+requests(Nodes, MyId) ->
     lists:map(
-      fun(P) -> 
-        R = make_ref(), 
-        P ! {request, self(), R}, 
-        R 
-      end, 
-      Nodes).
+        fun(P) ->
+            R = make_ref(),
+            P ! {request, self(), R, MyId},
+            R
+        end,
+        Nodes).
 
-wait(Nodes, Master, [], Waiting, TakeRef, Id) ->
+%STATE WAITING ALL OKs
+wait(Nodes, Master, [], Waiting, TakeRef, MyId) ->
     Master ! {taken, TakeRef},
-    held(Nodes, Waiting, Id);
+    held(Nodes, Waiting, MyId);
 
-wait(Nodes, Master, Refs, Waiting, TakeRef, Id) ->
+%%STATE WAITING
+wait(Nodes, Master, Refs, Waiting, TakeRef, MyId) ->
     receive
-        {request, From, Ref, FromId} ->
+        {request, From, Ref, Id} ->
             if
-                FromId < Id ->
-                    wait(Nodes, Master, Refs, [{From, Ref}|Waiting], TakeRef, Id),
+                Id < MyId -> From ! {ok, Ref},
                     R = make_ref(),
-                    {From, Ref} ! {ok, R};
-                Id > 0 -> 
-                    wait(Node,Master,Refs,Waiting,TakeRef, Id)     
-            end.
+                    From ! {request, self(), R, MyId},
+                    wait(Nodes, Master, [R|Refs], Waiting, TakeRef, MyId);
+                true -> wait(Nodes, Master, Refs, [{From, Ref}|Waiting], TakeRef, MyId)
+            end;
         {ok, Ref} ->
             NewRefs = lists:delete(Ref, Refs),
-            wait(Nodes, Master, NewRefs, Waiting, TakeRef, Id);
+
+            wait(Nodes, Master, NewRefs, Waiting, TakeRef, MyId);
         release ->
-            ok(Waiting),            
-            open(Nodes)
+            ok(Waiting),
+            open(Nodes, MyId)
     end.
 
 ok(Waiting) ->
     lists:map(
-      fun({F,R}) -> 
-        F ! {ok, R} 
-      end, 
-      Waiting).
+        fun({F,R}) ->
+            F ! {ok, R}
+        end,
+        Waiting).
 
-held(Nodes, Waiting, Id) ->
+held(Nodes, Waiting, MyId) ->
     receive
         {request, From, Ref} ->
-            held(Nodes, [{From, Ref}|Waiting], Id);
+            held(Nodes, [{From, Ref}|Waiting],MyId);
         release ->
             ok(Waiting),
-            open(Nodes, Id)
+            open(Nodes, MyId)
     end.
