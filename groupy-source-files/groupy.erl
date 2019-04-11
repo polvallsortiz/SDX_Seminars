005b29310c8608bcb867c40e49f0715613adcd48
Marc Guinovart Plana <marc.guinovart@c6s303pc22.fib.upc.es>
2019-04-11 11:47:20 +0200

    groupy s1

---
+++ groupy-source-files/groupy.erl
@@ -0,0 +1,36 @@
+-module(groupy).
+-export([start/2, stop/0]).
+
+% We use the name of the module (i.e. gms3) as the parameter Module to the start procedure. Sleep stands for up to how many milliseconds the workers should wait until the next message is sent.
+
+start(Module, Sleep) ->
+    P = worker:start("P1", Module, Sleep),
+    register(a, P), 
+    spawn('p1@127.0.0.1', fun() -> group_leader(whereis(user), self()),
+				register(b, worker:start("P2", Module, P, Sleep))
+				end),
+    spawn('p2@127.0.0.1',fun() -> group_leader(whereis(user), self()),
+				register(c, worker:start("P3", Module, P, Sleep))
+				end),
+    spawn('p3@127.0.0.1', fun() -> group_leader(whereis(user), self()),
+				register(d, worker:start("P4", Module, P, Sleep))
+				end),
+    spawn('p4@127.0.0.1', fun() -> group_leader(whereis(user), self()),
+				register(e, worker:start("P5", Module, P, Sleep))
+				end).
+
+stop() ->
+    stop(a),
+    stop(b),
+    stop(c),
+    stop(d),
+    stop(e).
+
+stop(Name) ->
+    case whereis(Name) of
+        undefined ->
+            ok;
+        Pid ->
+            Pid ! stop
+    end.
+
